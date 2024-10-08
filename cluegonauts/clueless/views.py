1ea2bb60f08a13ac10fefe21d93710f783316d2d
wmmcrury <bill@mcrury.com>
2024-10-08 15:45:05 -0400

    fast forward with remote feature branch

--- cluegonauts/clueless/views.py
+++ cluegonauts/clueless/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, "clueless/index.html")
 
-
+def gameb(request):
+    return render(request, "clueless/gameb.html")
