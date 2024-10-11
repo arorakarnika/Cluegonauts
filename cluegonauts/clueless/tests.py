84127b4fec2bc1bf2d4836675d02695fae88d765
bruszow1 <160781414+bruszow1@users.noreply.github.com>
2024-10-11 19:38:46 -0400

    Add Selenium and basic tests
    Adds Selenium
    Tests GamePlayersConsumer connection
    Tests player-log overflow behavior
    Minor bugfixes

--- cluegonauts/clueless/tests.py
+++ cluegonauts/clueless/tests.py
@@ -1,4 +1,73 @@
 # Use noqa: F401 to ignore unused import error
 from django.test import TestCase  # noqa: F401
+from channels.testing import WebsocketCommunicator
+from channels.testing import ChannelsLiveServerTestCase
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
+from clueless.consumers import GamePlayersConsumer
 
-# Create your tests here.
+
+class ConsumerTests(TestCase):
+    # Tests consumer's initial connection
+    async def test_quick_game_connection(self):
+        client1_communicator = WebsocketCommunicator(GamePlayersConsumer.as_asgi(),  r"ws/clueless/gameroom/$")
+        client1_connected, subprotocol = await client1_communicator.connect()
+        assert client1_connected
+
+        client2_communicator = WebsocketCommunicator(GamePlayersConsumer.as_asgi(), r"ws/clueless/gameroom/$")
+        client2_connected, subprotocol = await client2_communicator.connect()
+        assert client2_connected
+
+        await client1_communicator.send_json_to({'message': 'Miss Scarlett has joined the game.'})
+
+        client1_response = await client1_communicator.receive_json_from()
+        assert client1_response['message'] == 'Miss Scarlett has joined the game.'
+        client2_response = await client2_communicator.receive_json_from()
+        assert client2_response['message'] == 'Miss Scarlett has joined the game.'
+
+        await client1_communicator.disconnect()
+        await client2_communicator.disconnect()
+
+
+class SeleniumTests(ChannelsLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(2)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    # Verify chat log displays messages
+    def test_chat_message_display(self):
+        self.selenium.get(f"{self.live_server_url}/clueless/")
+
+        initial_player_log_str = self.selenium.execute_script("return document.querySelector('#player-log').innerHTML") # stores all html inside div with id player-log
+        selected_player = self.selenium.execute_script("return document.querySelector('#player-select').value")
+
+        self.selenium.find_element(By.ID, "player-submit").click()
+
+        modified_player_log_str = self.selenium.execute_script("return document.querySelector('#player-log').innerHTML") # stores all html inside div with id player-log
+        player_log_difference_str = modified_player_log_str.removeprefix(initial_player_log_str)
+
+        self.assertEqual("{} has joined the game.".format(selected_player) in player_log_difference_str, True)
+
+    # Verify chat log doesn't grow larger than browser
+    def test_chat_message_overflow(self):
+        self.selenium.get(f"{self.live_server_url}/clueless/")
+        self.selenium.maximize_window()
+        screen_height = self.selenium.get_window_size()['height']
+
+        initial_height = self.selenium.find_element(By.ID, "player-log").size['height']
+        self.selenium.find_element(By.ID, "player-submit").click()
+        message_size = self.selenium.find_element(By.ID, "player-log").size['height'] - initial_height
+        screen_message_capacity = int(screen_height / message_size)
+
+        for num in range(0, screen_message_capacity + 1):
+            self.selenium.find_element(By.ID, "player-submit").click()
+        player_log_height = self.selenium.find_element(By.ID, "player-log").size['height']
+
+        self.assertLessEqual(player_log_height, screen_height)
